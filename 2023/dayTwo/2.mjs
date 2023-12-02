dae99f4437960fe983f9cb665f84be6e1c2bea21
dna737 <adnanhere27@gmail.com>
2023-12-02 11:41:01 -0600

    complete part 1

--- 2023/dayTwo/2.mjs
+++ 2023/dayTwo/2.mjs
@@ -1,6 +1,35 @@
 import { open } from "node:fs/promises";
 
+let result = 0;
+
 const file = await open("./input.txt");
 for await (const line of file.readLines()) {
     //call the primary function here.
+    if (validateCubes(line)) {
+        result += getGameID(line);
+    }
+    console.log(result);
+}
+
+function getGameID(line) {
+    return parseInt(line.split(":")[0].split(" ")[1]);
+}
+
+function validateCubes(line) {
+    const maxNumCubes = { red: 12, green: 13, blue: 14 };
+    const totalCubesShown = line.split(":")[1];
+    for (const cubesShown of totalCubesShown.split(";")) {
+        console.log("cubesShown", cubesShown);
+        for (const cube of cubesShown
+            .replaceAll(/, /gi, ",")
+            .trim()
+            .split(",")) {
+            if (
+                maxNumCubes[cube.split(" ")[1]] < parseInt(cube.split(" ")[0])
+            ) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
