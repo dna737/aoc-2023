f6b21e3022ad57c02e7ad16f5d091672d922619f
dna737 <adnanhere27@gmail.com>
2023-12-02 13:59:06 -0600

    complete d2p2

--- 2023/dayTwo/2.mjs
+++ 2023/dayTwo/2.mjs
@@ -1,15 +1,16 @@
 import { open } from "node:fs/promises";
 
 let result = 0;
-
+let power = 0;
 const file = await open("./input.txt");
 for await (const line of file.readLines()) {
     //call the primary function here.
     if (validateCubes(line)) {
         result += getGameID(line);
     }
-    console.log(result);
+    power += getPower(line);
 }
+console.log("p1 && p2:", result, power);
 
 function getGameID(line) {
     return parseInt(line.split(":")[0].split(" ")[1]);
@@ -19,7 +20,6 @@ function validateCubes(line) {
     const maxNumCubes = { red: 12, green: 13, blue: 14 };
     const totalCubesShown = line.split(":")[1];
     for (const cubesShown of totalCubesShown.split(";")) {
-        console.log("cubesShown", cubesShown);
         for (const cube of cubesShown
             .replaceAll(/, /gi, ",")
             .trim()
@@ -32,4 +32,33 @@ function validateCubes(line) {
         }
     }
     return true;
+}
+
+function getPower(line) {
+    let answer = 1;
+    let maxCubesShown = {};
+    const totalCubesShown = line.split(":")[1];
+    for (const cubes of totalCubesShown
+        .trim()
+        .replaceAll(/, /gi, ",")
+        .replaceAll(/; /gi, ";")
+        .split(";")) {
+        for (const cube of cubes.split(",")) {
+            const numCubes = parseInt(cube.split(" ")[0]);
+            const cubeColor = cube.split(" ")[1];
+            if (Object.keys(maxCubesShown).includes(cubeColor)) {
+                if (maxCubesShown[cubeColor] < numCubes) {
+                    maxCubesShown[cubeColor] = numCubes;
+                }
+            } else {
+                maxCubesShown[cubeColor] = numCubes;
+            }
+        }
+    }
+
+    for (const value of Object.values(maxCubesShown)) {
+        answer *= value;
+    }
+
+    return answer;
 }
