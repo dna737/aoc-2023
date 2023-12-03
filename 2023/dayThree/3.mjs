48494e44b6ebb6b386fddd1bab332ef623123eb8
dna737 <adnanhere27@gmail.com>
2023-12-03 16:18:37 -0600

    complete d3p1

--- 2023/dayThree/3.mjs
+++ 2023/dayThree/3.mjs
@@ -1,38 +1,63 @@
+/*
+p1 logic: going through the "surroundings" of a number, and using regex to check if it's valid or not.
+
+
+*/
 import fs from "fs";
-const contents = fs.readFileSync("./simpleInput.txt", {
+const contents = fs.readFileSync("./input.txt", {
     encoding: "utf8",
     flag: "r",
 });
+const contentsArr = contents.split("\n");
 
-const symbols = ["*", "$", "#", "+"];
-let result = 0;
+let result = 0,
+    nonSymbols = /^[\d\.]/g,
+    prevLine = "",
+    nextLine = "";
 
-let lineIndex = 0;
-for (const line of contents.split("\n")) {
-    result += extractDigits(line, contents, lineIndex);
-    lineIndex++;
+for (let i = 0; i < contentsArr.length; i++) {
+    if (i !== 0) {
+        prevLine = contentsArr[i - 1];
+    } else {
+        prevLine = "";
+    }
+
+    if (i !== contentsArr.length - 1) {
+        nextLine = contentsArr[i + 1];
+    } else {
+        nextLine = "";
+    }
+
+    result += extractValidDigits(contentsArr[i], prevLine, nextLine);
 }
-console.log("result:", result);
 
-function extractDigits(line, contents, lineIndex) {
-    const numberOccurrences = [...line.matchAll(/\d+/g)];
+function extractValidDigits(currLine, prevLine, nextLine) {
+    const numberOccurrences = [...currLine.matchAll(/\d+/g)];
     let currResult = 0;
     for (const occ of numberOccurrences) {
-        let number = occ[0];
-        let currIndex = occ.index;
+        currResult += checkSurroundings(occ, currLine, prevLine, nextLine);
+    }
+    return currResult;
+}
 
-        //check if it's right above:
-        if (lineIndex > 0) {
-            console.log(contents.split("\n")[lineIndex - 1][currIndex]);
-        }
+function checkSurroundings(occ, currLine, prevLine, nextLine) {
+    const currStr = occ[0];
+    let startNumber = occ.index - 1,
+        endNumber = occ.index + currStr.length;
 
-        //check if the symbol is right below:
-        if (lineIndex !== contents.split("\n").length - 1) {
-            let currItem = contents.split("\n")[lineIndex + 1][currIndex];
-            if (symbols.includes(currItem)) {
-                currResult += parseInt(number);
+    let xRanges = Array.from(
+        { length: endNumber - startNumber + 1 },
+        (_, index) => startNumber + index
+    ).filter((x) => x >= 0 && x <= contents.split("\n")[0].length - 1);
+
+    const linesArray = [currLine, prevLine, nextLine];
+
+    for (const line of linesArray) {
+        for (const xCoord of xRanges) {
+            if (line[xCoord] && line[xCoord].match(/[^0-9.]/)) {
+                return parseInt(currStr);
             }
         }
     }
-    return currResult;
+    return 0;
 }
